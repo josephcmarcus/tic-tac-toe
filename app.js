f3721d5a6514a97e8776f0eae0af8cecdacf1d66
Joe Marcus <josephcmarcus@gmail.com>
2022-07-16 23:53:45 -0400

    Rework marker placement, pattern calculation and score check

--- app.js
+++ app.js
@@ -1,92 +1,154 @@
 (() => {
     const container = document.getElementById('container');
-
     container.addEventListener('click', (event) => {
         if (event.target.className != 'box') {
             return;
         } else {
-            let clickLocation = event.target.id;
-            gameBoard.addMarker(clickLocation);
-        }
+            gameBoard.addMarker(event.target.id);
+        };
     });
 })();
 
 const gameBoard = (() => {
     const _markers = {
-        markerX: 'X',
-        markerO: 'O',
-        markerXValue: 1,
-        markerOValue: -1,
-        currentMarker: '',
-        currentMarkerValue: ''
-    }
-    const _positions = { tl: 0, tc: 0, tr: 0, ml: 0, mc: 0, mr: 0, bl: 0, bc: 0, br: 0 }
-    const { tl, tc, tr, ml, mc, mr, bl, bc, br } = _positions;
-    let _boardLayout = [
-        [tl, tc, tr],
-        [ml, mc, mr],
-        [bl, bc, br],
-    ];
-    const addMarker = function(location) {
-        if (gameState.playerTurnCheck() === 'playerOne') {
-            _markers.currentMarker = _markers.markerX;
-            _markers.currentMarkerValue = _markers.markerXValue;
-        } else {
-            _markers.currentMarker = _markers.markerO;
-            _markers.currentMarkerValue = _markers.markerOValue;
-        }
-        _positions[location] = _markers.currentMarkerValue;
-        switch (location) {
-            case 'tl':
-                _boardLayout[0][0] = _markers.currentMarkerValue;
-                break;
-            case 'tc':
-                _boardLayout[0][1] = _markers.currentMarkerValue;
-                break;
-            case 'tr':
-                _boardLayout[0][2] = _markers.currentMarkerValue;
-                break;
-            case 'ml':
-                _boardLayout[1][0] = _markers.currentMarkerValue;
-                break;
-            case 'mc':
-                _boardLayout[1][1] = _markers.currentMarkerValue;
-                break;
-            case 'mr':
-                _boardLayout[1][2] = _markers.currentMarkerValue;
-                break;
-            case 'bl':
-                _boardLayout[2][0] = _markers.currentMarkerValue;
-                break;
-            case 'bc':
-                _boardLayout[2][1] = _markers.currentMarkerValue;
-                break;
-            case 'br':
-                _boardLayout[2][2] = _markers.currentMarkerValue;
-                break;
-        }
-        gameState.playerSwitch();
+        x: 'X',
+        o: 'O',
+        xValue: 1,
+        oValue: -1,
+        current: '',
+        currentValue: ''
     };
-    const logger = function() {
-        console.log(_positions.tl);
-        console.table(_boardLayout)
-    }
+    let _layout = [
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0],
+    ];
+    const _logger = () => {
+        console.table(_layout)
+    };
+
+    const addMarker = function(location) {
+        if (gameState.playerTurnCheck() === 'playerOne') { 
+            _markers.current = _markers.x;
+            _markers.currentValue = _markers.xValue;
+        } else {
+            _markers.current = _markers.o;
+            _markers.currentValue = _markers.oValue;
+        }
+        _markerLocations[location](_markers.currentValue); 
+        gameState.playerSwitch(gameState.playerTurnCheck());
+        _logger();
+    };
+    // object serves as key for placing marker on square and calls score check on intersecting patterns
+    const _markerLocations = {
+        tl: (value) => {
+            _layout[0][0] = value;
+            gameState.scoreCheck([
+                gameState.calcPatterns.topRow, 
+                gameState.calcPatterns.leftCol, 
+                gameState.calcPatterns.ltrDiag
+            ]);
+        },
+        tc: (value) => {
+            _layout[0][1] = value;
+            gameState.scoreCheck([
+                gameState.calcPatterns.topRow,
+                gameState.calcPatterns.middleCol
+            ]);
+        },
+        tr: (value) => {
+            _layout[0][2] = value;
+            gameState.scoreCheck([
+                gameState.calcPatterns.topRow,
+                gameState.calcPatterns.rightCol,
+                gameState.calcPatterns.ltrDiag
+            ]);
+        },
+        ml: (value) => {
+            _layout[1][0] = value;
+            gameState.scoreCheck([
+                gameState.calcPatterns.middleRow,
+                gameState.calcPatterns.leftCol,
+                gameState.calcPatterns.middleCol
+            ]);
+        },
+        mc: (value) => {
+            _layout[1][1] = value;
+            gameState.scoreCheck([
+                gameState.calcPatterns.middleRow,
+                gameState.calcPatterns.middleCol,
+                gameState.calcPatterns.rtlDiag,
+                gameState.calcPatterns.ltrDiag
+            ]);
+        },
+        mr: (value) => {
+            _layout[1][2] = value;
+            gameState.scoreCheck([
+                gameState.calcPatterns.middleRow,
+                gameState.calcPatterns.rightCol
+            ]);
+        },
+        bl: (value) => {
+            _layout[2][0] = value;
+            gameState.scoreCheck(
+                [gameState.calcPatterns.bottomRow, 
+                gameState.calcPatterns.leftCol, 
+                gameState.calcPatterns.rtlDiag]
+            );
+        },
+        bc: (value) => {
+            _layout[2][1] = value;
+            gameState.scoreCheck([
+                gameState.calcPatterns.bottomRow,
+                gameState.calcPatterns.middleCol
+            ]);
+        },
+        br: (value) => {
+            _layout[2][2] = value;
+            gameState.scoreCheck([
+                gameState.calcPatterns.bottomRow,
+                gameState.calcPatterns.rightCol,
+                gameState.calcPatterns.rtlDiag
+            ]);
+        }
+    };
+    const layoutCheck = () => _layout;
+
     return {
         addMarker,
-        logger
+        layoutCheck,
     };
 })();
 
 const gameState = (() => {
     let _playerTurn = 'playerOne';
-    const playerTurnCheck = function() {
-        return _playerTurn;
-    }
-    const playerSwitch = function() {
-        return _playerTurn === 'playerOne' ? _playerTurn = 'playerTwo' : _playerTurn = 'playerOne';
-    }
+    const _gameOver = (player) => player === 'playerOne' ? alert('playerOne Wins!') : alert('playerTwo Wins!')
+    const playerTurnCheck = () => _playerTurn
+    const playerSwitch = (player) => player === 'playerOne' ? _playerTurn = 'playerTwo' : _playerTurn = 'playerOne';
+    
+    // object calculates patterns on board for scoreCheck function
+    const calcPatterns = {
+        topRow: () => gameBoard.layoutCheck()[0].reduce((pre, curr) => pre + curr, 0), 
+        middleRow: () => gameBoard.layoutCheck()[1].reduce((pre, curr) => pre + curr, 0),
+        bottomRow: () => gameBoard.layoutCheck()[2].reduce((pre, curr) => pre + curr, 0),
+        leftCol: () => gameBoard.layoutCheck()[0][0] + gameBoard.layoutCheck()[1][0] + gameBoard.layoutCheck()[2][0],
+        middleCol: () => gameBoard.layoutCheck()[0][1] + gameBoard.layoutCheck()[1][1] + gameBoard.layoutCheck()[2][1],
+        rightCol: () => gameBoard.layoutCheck()[0][2] + gameBoard.layoutCheck()[1][2] + gameBoard.layoutCheck()[2][2],
+        ltrDiag: () => gameBoard.layoutCheck()[0][0] + gameBoard.layoutCheck()[1][1] + gameBoard.layoutCheck()[2][2],
+        rtlDiag: () => gameBoard.layoutCheck()[0][2] + gameBoard.layoutCheck()[1][1] + gameBoard.layoutCheck()[2][0]
+    };
+    const scoreCheck = (patterns) => {
+        patterns.map(function(pattern) {
+            console.log(pattern())
+            if (pattern() === 3 || pattern() === -3) {
+                _gameOver(_playerTurn);
+            };
+        });
+    };
     return {
+        calcPatterns,
+        playerSwitch,
         playerTurnCheck,
-        playerSwitch
+        scoreCheck
     }
 })();
